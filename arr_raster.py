a6f26966cb6fd9f5c0e7c306ba5c77db1121bff5
Alejandro Vidales <alej.vid98@gmail.com>
2022-01-13 14:09:19 -0600

    Dia inicial

--- arr_raster.py
+++ arr_raster.py
@@ -8,55 +8,96 @@ from constants import ALEJANDRO, BANDS, PRODUCTS
 from matplotlib import pyplot as plt
 from pathlib import Path
 
-
-
 #ruta = '/Users/nazariocano/Desktop/PYTHON'
-
 #RES = load_landsat_image(ruta,['BO3'], '2021', '12', '5')
 
-
-mes_inicial=12
-mes_final=12
-
 def months(inicial,final):
-    ruta = 'C:/Users/PC/S10/2021/*/'
+    fecha_init=re.findall('([A-Z0-9]{1,4})',inicial)
+    year_init=int(fecha_init[0])
+    month_init=int(fecha_init[1])
+    day_init=int(fecha_init[2])
+
+    fecha_fin=re.findall('([A-Z0-9]{1,4})',final)
+    year_fin=int(fecha_fin[0])
+    month_fin=int(fecha_fin[1])
+    day_fin=int(fecha_fin[2])
+
+    ruta = f'C:/Users/PC/S10/{year_init}/*/'
     fechas = glob.glob(ruta)
     meses=[]
+    
     try:
         for fecha in fechas:
             mes = int(re.findall('[0-9]+',fecha)[2])
             meses.append(mes)
             meses.sort()
 
-        if inicial in meses:
-            indice_in=meses.index(inicial)
-        if final in meses:
-            indice_fin=meses.index(final)+1
+        if month_init in meses:
+            indice_in=meses.index(month_init)
+
+        if month_fin in meses:
+            indice_fin=meses.index(month_fin)+1
 
         rango= meses[indice_in:indice_fin]   
-        return(rango) 
-    except:
-        print('Error')
+        return(rango,year_init,day_init) 
 
-def days(rangos):
+    except:
+        print('Error, no se encontraron fechas disponibles')
+
+def days(rango,year,day_init):
+    #print(rango,year,day_init)
+    i=0
+    num_meses=len(rango)
+    #print(num_meses)
     try:
-        for rango in rangos:
-            ruta = f'C:/Users/PC/S10/2021/{rango}/*/'
+        for rang in rango:
+            #print(rang)
+            ruta = f'C:/Users/PC/S10/{year}/{rang}/*/'
             fechas = glob.glob(ruta)
             dias=[]
+            aux_dias=[]
+            if  i==0:  
+                i=i+ 1
+                #print('Entro al if')                      
+                for fecha in fechas:
+                    dia = int(re.findall('[0-9]+',fecha)[3])
+                    dias.append(dia)
+                    dias.sort()
+                    if day_init in dias:
+                        indice=dias.index(day_init)
+                        #print(indice)
+                x=dias[indice:]
+                aux_dias=(x)
+                #dias.sort()
+                #print(aux_dias)
+
+                    #f.update({rang:dias})                
+            else:
+                #print('Entro al else')
+                for fecha in fechas:
+                    dia = int(re.findall('[0-9]+',fecha)[3])
+                    aux_dias.append(dia)
+                    aux_dias.sort() 
+                #print(aux_dias)
+
+            print(aux_dias)
+
             
-            for fecha in fechas:
-                dia = int(re.findall('[0-9]+',fecha)[3])
-                dias.append(dia)
-                dias.sort()
-            print (dias)    
+            #print(i)
     except:
-        print('No hay fechas') 
-
-#y=months(mes_inicial,mes_final)
-#days(y)
+     print('No hay fechas') 
 
 
+mes_inicial=12
+mes_final=12
+fecha_inicial='2021-5-15'
+fecha_final='2021-9-1'
+
+
+
+y=months(fecha_inicial,fecha_final)
+d=days(y[0],y[1],y[2])
+print(d)
 ######
 """
 dia=days(months(mes_inicial,mes_final))
@@ -65,6 +106,7 @@ mes=12
 path=ALEJANDRO
 banda=['B03']
 """
+
 def load_landsat_image(ruta, bands, year, mes, dia):
     image = {}
     path = Path(ruta)
